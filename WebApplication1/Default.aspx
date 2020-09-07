6079747a4e9695ae1f5b607341b1d61085113b3c
M.p <446017@cognizant.com>
2020-09-07 21:47:58 +0530

    Add project files.

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            This is a test app.
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="null"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SecurityProtocolType s = ServicePointManager.SecurityProtocol;
+
+            Label1.Text = "ServicePointManager.SecurityProtocol: " + ServicePointManager.SecurityProtocol.ToString();
+        }
+    }
+}
